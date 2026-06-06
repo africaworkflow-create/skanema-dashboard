5ab4e9dcd31762debb5529d59d22df9f0d200d13
africaworkflow-create <africaworkflow@gmail.com>
2026-06-06 03:25:10 +0000

    Fix Qr code redirection

--- app/q/[code]/page.jsx
+++ app/q/[code]/page.jsx
@@ -3,13 +3,17 @@ import { redirect } from 'next/navigation'
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.skanema.com'
 
 export default async function QRRedirectPage({ params }) {
-  const { code } = params
+  const { code } = await params
 
-  try {
-    const res = await fetch(`${API_URL}/api/qr/redirect/${code}`, { cache: 'no-store' })
-    const data = await res.json()
-    if (data.url) redirect(data.url)
-  } catch (_) {}
-
+  const res  = await fetch(`${API_URL}/api/qr/redirect/${code}`, { 
+    cache: 'no-store',
+    next : { revalidate: 0 }
+  })
+  const data = await res.json()
+  
+  if (data?.url && data.url !== 'https://www.skanema.com') {
+    redirect(data.url)
+  }
+  
   redirect('https://www.skanema.com')
 }
